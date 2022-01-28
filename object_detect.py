3a85ca7a357b46c47c1039fc5db334dcc8c5d8f4
blaxovios <me2055@unipi.gr>
2022-01-28 13:35:19 +0200

    ver. 0.0.1

--- object_detect.py
+++ object_detect.py
@@ -2,14 +2,26 @@ from pdf2image import convert_from_path
 import os
 import pytesseract
 import cv2
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+import pandas as pd
 
+'''With pytesseract library we also need to install tesseract-ocr for windows. 
+More info: https://github.com/UB-Mannheim/tesseract/wiki.
+Then, we need to download language data set from: https://github.com/tesseract-ocr/langdata
+and test data set from: https://github.com/tesseract-ocr/tessdata.
+Unzip the file and copy-paste the folders to tesseract installation path;
+e.x. C:\Program Files\Tesseract-OCR
+test data and language data folders must have the names "langdata" and "tessdata".
+Finally, configure tessdata path as shown below and use it as a parameter when tesseract is called in the code.'''
+
+tessdata_dir_config = '--tessdata-dir "C:/Program Files/Tesseract-OCR/tessdata"'
+# Set the tesseract path in the code
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 # Poppler dependecy is needed for pdf2image to work
 poppler = r'C:\Program Files (x86)\poppler-0.68.0\bin'
+
+# Extract images from pdf and save to a directory
 pages = convert_from_path(r'C:\Users\tsepe\PycharmProjects\objectDetectionNlp\app\static\pdfs\Κυλινδρόμυλοι Α.Ε. κλπ. απόφαση 954 ΚΠολΔ.pdf',
                           500, poppler_path=poppler)
-
-# Extracte images from pdf and save to a directory
 count = 0
 for page in pages:
     page.save('C:/Users/tsepe/PycharmProjects/objectDetectionNlp/app/static/pages/'+'page%d.jpg' % count, 'JPEG')
@@ -31,8 +43,15 @@ def get_filepaths(directory):
 full_file_paths = get_filepaths(directory)
 print(full_file_paths)
 
+pages_to_text_list = []
 for path in full_file_paths:
     img = cv2.imread(path)
+    if img is None:
+        pages_to_text_list.append("Could not read the image.")
 
-    img = cv2.resize(img, (600, 360))
-    print(pytesseract.image_to_string(img))
+    else:
+        rev = pytesseract.image_to_string(img, lang="ell", config=tessdata_dir_config)
+        pages_to_text_list.append(rev)
+print(pages_to_text_list)
+df_pages_to_text = pd.DataFrame(pages_to_text_list, columns=['Description'])
+df_pages_to_text.to_excel(r'C:\Users\tsepe\PycharmProjects\objectDetectionNlp\app\static\results\pages_to_text.xlsx', encoding='utf-8', index=False)

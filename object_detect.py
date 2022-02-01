bd834cffc37a53faf5c35ad5a5b1430f48afc347
blaxovios <me2055@unipi.gr>
2022-02-01 17:31:35 +0200

    ver. 0.0.2

--- object_detect.py
+++ object_detect.py
@@ -2,6 +2,7 @@ from pdf2image import convert_from_path
 import os
 import pytesseract
 import cv2
+import time
 import pandas as pd
 
 '''With pytesseract library we also need to install tesseract-ocr for windows. 
@@ -13,23 +14,35 @@ e.x. C:\Program Files\Tesseract-OCR
 test data and language data folders must have the names "langdata" and "tessdata".
 Finally, configure tessdata path as shown below and use it as a parameter when tesseract is called in the code.'''
 
+# Set the tesseract test data path in the code
 tessdata_dir_config = '--tessdata-dir "C:/Program Files/Tesseract-OCR/tessdata"'
 # Set the tesseract path in the code
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
 # Poppler dependecy is needed for pdf2image to work
 poppler = r'C:\Program Files (x86)\poppler-0.68.0\bin'
-
 # Extract images from pdf and save to a directory
-pages = convert_from_path(r'C:\Users\tsepe\PycharmProjects\objectDetectionNlp\app\static\pdfs\Κυλινδρόμυλοι Α.Ε. κλπ. απόφαση 954 ΚΠολΔ.pdf',
-                          500, poppler_path=poppler)
-count = 0
-for page in pages:
-    page.save('C:/Users/tsepe/PycharmProjects/objectDetectionNlp/app/static/pages/'+'page%d.jpg' % count, 'JPEG')
-    count += 1
-print('Pages were extracted from pdf')
+pdf_path = 'C:/Users/kostas.skepetaris/PycharmProjects/objectDetectionNlp/app/static/pdfs/Κυλινδρόμυλοι Α.Ε. κλπ. απόφαση 954 ΚΠολΔ.pdf'
+start1 = time.time()
 
+
+def convert_pdf(pdf_path, poppler):
+    pages = convert_from_path(pdf_path, 500, poppler_path=poppler)
+    count = 0
+    for page in pages:
+        page.save(
+            'C:/Users/kostas.skepetaris/PycharmProjects/objectDetectionNlp/app/static/pages/' + 'page%d.jpg' % count,
+            'JPEG')
+        count += 1
+
+
+convert_pdf(pdf_path, poppler)
+end1 = time.time()
+print("Pdf to image conversion took {:.1f} seconds".format(end1 - start1))
 # Save full paths of pages in a list
-directory = ('C:/Users/tsepe/PycharmProjects/objectDetectionNlp/app/static/pages/')
+directory = 'C:/Users/kostas.skepetaris/PycharmProjects/objectDetectionNlp/app/static/pages/'
+
+
 def get_filepaths(directory):
     file_paths = []  # List which will store all of the full filepaths.
     # Walk the tree.
@@ -40,18 +53,43 @@ def get_filepaths(directory):
             file_paths.append(filepath)  # Add it to the list.
 
     return file_paths
+
+
 full_file_paths = get_filepaths(directory)
 print(full_file_paths)
 
-pages_to_text_list = []
-for path in full_file_paths:
-    img = cv2.imread(path)
-    if img is None:
-        pages_to_text_list.append("Could not read the image.")
+text_name_from_pdf = 'C:/Users/kostas.skepetaris/PycharmProjects/objectDetectionNlp/app/static/texts/' + \
+                     os.path.splitext(os.path.basename(pdf_path))[0] + '.txt'
+start2 = time.time()
 
-    else:
-        rev = pytesseract.image_to_string(img, lang="ell", config=tessdata_dir_config)
-        pages_to_text_list.append(rev)
-print(pages_to_text_list)
-df_pages_to_text = pd.DataFrame(pages_to_text_list, columns=['Description'])
-df_pages_to_text.to_excel(r'C:\Users\tsepe\PycharmProjects\objectDetectionNlp\app\static\results\pages_to_text.xlsx', encoding='utf-8', index=False)
+
+def image_to_text(text_name_from_pdf):
+    # A text file is created and flushed
+    txt_file = open(text_name_from_pdf, "w+", encoding="utf-8")
+    txt_file.write("")
+    txt_file.close()
+    # OpenCV will handle image processing
+    for path in full_file_paths:
+        img = cv2.imread(path)
+        # Convert the image to gray scale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if img is None:
+            txt_file = open(text_name_from_pdf, "w", encoding="utf-8")
+            text = "---Could not read the image.---"
+            txt_file.write(text)
+            txt_file.close()
+        else:
+            txt_file = open(text_name_from_pdf, "w", encoding="utf-8")
+            text = pytesseract.image_to_string(gray, lang="ell", config=tessdata_dir_config)
+            txt_file.write(text)
+            txt_file.close()
+    file = open(txt_file, "w+", encoding="utf-8")
+    print(file.read())
+    file.close()
+    return file
+
+
+image_to_text(text_name_from_pdf)
+end2 = time.time()
+print("Image to text conversion took {:.1f} seconds".format(end2 - start2))
+print('Pdf was extracted to text in'.format(end2 - start1))

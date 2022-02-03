8317b0092869a83214748a6659ce5eabafd9d2de
blaxovios <me2055@unipi.gr>
2022-02-03 17:49:10 +0200

    ver. 0.0.2

--- object_detect.py
+++ object_detect.py
@@ -4,10 +4,12 @@ import os
 import pytesseract
 import cv2
 import time
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from keras.layers import Input, Dense, Embedding, Conv2D, MaxPool2D
+from keras.layers import Reshape, Flatten, Dropout, Concatenate
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+from keras.models import Model
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 
 """With pytesseract library we also need to install tesseract-ocr for windows. 
 More info: https://github.com/UB-Mannheim/tesseract/wiki.
@@ -112,40 +114,59 @@ end2 = time.time()
 print("Image to text conversion took {:.1f} seconds".format(end2 - start2))
 print('Pdf was extracted to text in'.format(end2 - start1))
 
-file_for_dict = os.path.splitext(os.path.basename(pdf_path))[0]+'.txt'
-# Extract the folder into a data folder and go ahead and load the data with Pandas
-filepath_dict = {file_for_dict: text_name_from_pdf}
-df_list = []
-for source, filepath in filepath_dict.items():
-    df = pd.read_csv(filepath, names=['text', 'label'], sep='\t')
-    df['source'] = source  # Add another column filled with the source name
-    df_list.append(df)
+print('Loading data')
+file = open(text_name_from_pdf).read()
 
-df = pd.concat(df_list)
-print(df.iloc[0]) # Use [:] to print all text inside txt file
+x, y, vocabulary, vocabulary_inv = file
 
-# Use the CountVectorizer provided by the scikit-learn library to vectorize sentences
-vectorizer = CountVectorizer(min_df=0, lowercase=False)
-vectorizer.fit(df.iloc[:])
-vectorized_vocabulary = vectorizer.vocabulary_
-print(vectorized_vocabulary)
-vectorized_transform = vectorizer.transform(df.iloc[:]).toarray()
-print(vectorized_transform)
+# x.shape -> (10662, 56)
+# y.shape -> (10662, 2)
+# len(vocabulary) -> 18765
+# len(vocabulary_inv) -> 18765
 
-# Defining a Baseline Model
-df_txt = df[df['source'] == file_for_dict]
-sentences = df_txt['text'].values
-y = df_txt['label'].values
-sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=100)
-vectorizer = CountVectorizer()
-vectorizer.fit(sentences_train)
-X_train = vectorizer.transform(sentences_train)
-X_test  = vectorizer.transform(sentences_test)
-print(X_train)
+X_train, X_test, y_train, y_test = train_test_split( x, y,     test_size=0.2, random_state=42)
 
-# The classification model we are going to use is the logistic regression which is a simple yet powerful linear model
-# that is mathematically speaking in fact a form of regression between 0 and 1 based on the input feature vector.
-classifier = LogisticRegression()
-classifier.fit(X_train, y_train)
-score = classifier.score(X_test, y_test)
-print("Accuracy:", score)
+# X_train.shape -> (8529, 56)
+# y_train.shape -> (8529, 2)
+# X_test.shape -> (2133, 56)
+# y_test.shape -> (2133, 2)
+
+
+sequence_length = x.shape[1] # 56
+vocabulary_size = len(vocabulary_inv) # 18765
+embedding_dim = 256
+filter_sizes = [3,4,5]
+num_filters = 512
+drop = 0.5
+
+epochs = 100
+batch_size = 30
+
+# this returns a tensor
+print("Creating Model...")
+inputs = Input(shape=(sequence_length,), dtype='int32')
+embedding = Embedding(input_dim=vocabulary_size,     output_dim=embedding_dim, input_length=sequence_length)(inputs)
+reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
+
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0],     embedding_dim), padding='valid', kernel_initializer='normal',     activation='relu')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1],     embedding_dim), padding='valid', kernel_initializer='normal',     activation='relu')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2],     embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
+dropout = Dropout(drop)(flatten)
+output = Dense(units=2, activation='softmax')(dropout)
+
+# this creates a model that includes
+model = Model(inputs=inputs, outputs=output)
+
+checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5',     monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+print("Traning Model...")
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint], validation_data=(X_test, y_test))  #     starts training

9df7568092a59597f70a17faf8a91ed8332bc9a7
blaxovios <me2055@unipi.gr>
2022-03-22 18:10:53 +0200

    -ver 0.2 || With the use of spacy we will first manually make it detect entities based on our own category of words, leveraging NER to identify them.

--- nlp.py
+++ nlp.py
@@ -1,68 +1,62 @@
-import nltk
-from nltk import word_tokenize
-from nltk.probability import FreqDist
-import urllib.request
-from matplotlib import pyplot as plt
-nltk.download('punkt')
-nltk.download("stopwords")
-from nltk.corpus import stopwords
+"""Custom Named Entity Recognition (NER) and feature extraction with NLP
+This module reads xlsx file that contains all text from auction files downloaded as pdf from deltio nomikon. We will manually
+make it detect entities based on our own category of words, leveraging NER to identify them. Annotating the data,
+preparation of the training data, training a custom spaCy v3 model (previous versions of spacy differ a lot) and assess
+model results are all the necessary steps.
+Spacy will be used for those processes.
+
+Author: Kostas
+
+Created: 22nd March, 2022
+"""
+# Imports
 import pandas as pd
+import spacy
+from spacy import displacy
+from spacy.tokens import DocBin
+import json
+from datetime import datetime
+from tqdm import tqdm
+import re
+
+# With the help of: https://levelup.gitconnected.com/auto-detect-anything-with-custom-named-entity-recognition-ner-c89d6562e8e9
 
 text_filepath = 'C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/results/deltio_nomikon/deltio 08-03-2022.xlsx'
 text_df = pd.read_excel(text_filepath)
-
 labels_df = pd.read_excel('C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/csv/Weekly update 11012022_17022022.xlsx')
 labels_list = list(labels_df.columns)
 text_df = text_df.reindex(columns=[*text_df.columns.tolist(), 'labels'], fill_value=str(labels_list))
 text_description_labels_df = text_df[['Asset Description', 'labels']]
 text_description_labels_df = text_description_labels_df[~text_description_labels_df['Asset Description'].duplicated()]
 
-#tokenize text by words
-words = word_tokenize(text_description_labels_df['Asset Description'][0])
-#check the number of words
-print(f"The total number of words in the text is {len(words)}")
-#find the frequency of words
-fdist = FreqDist(words)
-#print the 10 most common words
-fdist.most_common(10)
-#create an empty list to store words
-words_no_punc = []
-#iterate through the words list to remove punctuations
-for word in words:
-    if word.isalpha():
-        words_no_punc.append(word.lower())
-#print number of words without punctuation
-print(f"The total number of words without punctuation is {len(words_no_punc)}")
-#find the frequency of words
-fdist = FreqDist(words_no_punc)
-#Plot the 10 most common words
-fdist.plot(10)
-plt.show()
-#list of stopwords
-stopwords_list = stopwords.words("greek")
-print(stopwords_list)
-#create an empty list to store clean words
-clean_words = []
-#Iterate through the words_no_punc list and add non stopwords to the new clean_words list
-for word in words_no_punc:
-    if word not in stopwords_list:
-        clean_words.append(word)
-print(f"The total number of words without punctuation and stopwords is {len(clean_words)}")
-#find the frequency of words
-fdist = FreqDist(clean_words)
-#Plot the 10 most common words
-fdist.plot(10)
-plt.show()
-#Update the stopwords list
-stopwords_list.extend(["said","one","like","came","back"])
-#create an empty list to store clean words
-clean_words = []
-#Iterate through the words_no_punc list and add non stopwords to the new clean_words list
-for word in words_no_punc:
-    if word not in stopwords_list:
-        clean_words.append(word)
-#find the frequency of words
-fdist = FreqDist(clean_words)
-#Plot the 10 most common words
-fdist.plot(10)
-plt.show()
+# this dictionary will contain all annotated examples
+collective_dict = {'TRAINING_DATA': []}
+
+
+def structure_training_data(text, kw_list):
+	results = []
+	entities = []
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "SERVICE"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# add any found entities into a JSON format within collective_dict
+	if len(entities) > 0:
+		results = [text, {"entities": entities}]
+		collective_dict['TRAINING_DATA'].append(results)
+		return

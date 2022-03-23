ad9295553a296020adc955abc691f70eb735f009
blaxovios <me2055@unipi.gr>
2022-03-23 17:33:20 +0200

    -ver 0.2 || With the use of spacy we will first manually make it detect entities based on our own category of words, leveraging NER to identify them. Try to make opencv give better results with the text

--- nlp.py
+++ nlp.py
@@ -18,27 +18,45 @@ import json
 from datetime import datetime
 from tqdm import tqdm
 import re
+from os import listdir
+from os.path import isfile, join
+import os
+from IPython.core.display import display, HTML
+
 
 # With the help of: https://levelup.gitconnected.com/auto-detect-anything-with-custom-named-entity-recognition-ner-c89d6562e8e9
 
-text_filepath = 'C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/results/deltio_nomikon/deltio 08-03-2022.xlsx'
+'''text_filepath = 'C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/results/deltio_nomikon/deltio 08-03-2022.xlsx'
 text_df = pd.read_excel(text_filepath)
 labels_df = pd.read_excel('C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/csv/Weekly update 11012022_17022022.xlsx')
 labels_list = list(labels_df.columns)
 text_df = text_df.reindex(columns=[*text_df.columns.tolist(), 'labels'], fill_value=str(labels_list))
 text_description_labels_df = text_df[['Asset Description', 'labels']]
 text_description_labels_df = text_description_labels_df[~text_description_labels_df['Asset Description'].duplicated()]
+asset_description_df = text_description_labels_df['Asset Description']'''
+
+mypath = "C:\\Users\\kostas.skepetaris\\PycharmProjects\\web_retrieve_data\\static\\texts"
+txt_filepaths = []
+def absoluteFilePaths(directory):
+    for dirpath,_,filenames in os.walk(directory):
+        for f in filenames:
+            txt_filepaths.append(os.path.abspath(os.path.join(dirpath, f)))
+	return txt_filepaths
+
+absoluteFilePaths(mypath)
 
 # this dictionary will contain all annotated examples
 collective_dict = {'TRAINING_DATA': []}
 
 
-def structure_training_data(text, kw_list):
+def structure_training_data(text, kw_auction_date_list, kw_tax_id_of_debtor_list, kw_auction_id_list, kw_eauction_website_list,
+							kw_foreclosure_id_list, kw_accelerator_list, kw_tax_id_accelerator_list, kw_property_type_list,
+							kw_size_of_land_list, kw_percentage_of_ownership_list, kw_notary_public_list):
 	results = []
 	entities = []
 
 	# search for instances of keywords within the text (ignoring letter case)
-	for kw in tqdm(kw_list):
+	for kw in tqdm(kw_auction_date_list):
 		search = re.finditer(kw, text, flags=re.IGNORECASE)
 
 		# store the start/end character positions
@@ -49,7 +67,187 @@ def structure_training_data(text, kw_list):
 			for i in all_instances:
 				start = i[0]
 				end = i[1]
-				entities.append((start, end, "SERVICE"))
+				entities.append((start, end, "auction_date"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_tax_id_of_debtor_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "tax_id_of_debtor"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_auction_id_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "auction_id"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_eauction_website_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "eauction_website"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_foreclosure_id_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "foreclosure_id"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_accelerator_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "accelerator"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_tax_id_accelerator_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "tax_id_accelerator"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_property_type_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "property_type"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_size_of_land_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "size_of_land"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_percentage_of_ownership_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "percentage_of_ownership"))
+
+		# alert when no matches are found given the user inputs
+		else:
+			print("No pattern matches found. Keyword:", kw)
+
+	# search for instances of keywords within the text (ignoring letter case)
+	for kw in tqdm(kw_notary_public_list):
+		search = re.finditer(kw, text, flags=re.IGNORECASE)
+
+		# store the start/end character positions
+		all_instances = [[m.start(), m.end()] for m in search]
+
+		# if the callable_iterator found matches, create an 'entities' list
+		if len(all_instances) > 0:
+			for i in all_instances:
+				start = i[0]
+				end = i[1]
+				entities.append((start, end, "notary_public"))
 
 		# alert when no matches are found given the user inputs
 		else:
@@ -60,3 +258,150 @@ def structure_training_data(text, kw_list):
 		results = [text, {"entities": entities}]
 		collective_dict['TRAINING_DATA'].append(results)
 		return
+
+
+# Define training keywords and input text
+kw_auction_date_list = ['Ημερομηνία Διεξαγωγής Πλειστηριασμού: 23/03/2022']
+kw_tax_id_of_debtor_list = ['ΑΦΜ Οφειλέτη: 036044215']
+kw_auction_id_list = ['Μοναδικός Κωδικός']
+kw_eauction_website_list = ['ΙΣΤΟΣΕΛΙΔΑ ΔΗΜΟΣΙΕΥΣΕΩΝ ΠΛΕΙΣΤΗΡΙΑΣΜΩΝ']
+kw_foreclosure_id_list = ['έκθεση αναγκαστικής κατάσχεσης']
+kw_accelerator_list = ['Επισπεύδων']
+kw_tax_id_accelerator_list = ['ΑΦΜ Επισπεύδοντα']
+kw_property_type_list = ['διαμέρισμα', 'αποθήκη', 'μεζονέτα', 'μονοκατοικία', 'διώροφη οικοδομή διαμερισμάτων',
+						 'διώροφη οικοδομή', 'διόροφη οικοδομή', 'διαμερίσματα', 'καταστημάτων', 'καταστήματα',
+						 'Επαγγελματικό Κτίριο', 'Κτήριο', 'Ξενοδοχειακή Μονάδα', 'Αποθηκευτικός Χώρος', 'Βιομηχανικός Χώρος',
+						 'Βιοτεχνικός Χώρος', 'Αίθουσα', 'Εκθεσιακός Χώρος', 'Υπόγειο parking', 'Κλειστό parking',
+						 'Parking πιλοτής', 'χώροι σταύθμευσης', 'χώρος στάθμευσης', 'Ανοιχτό Parking', 'ξενοδοχείο',
+						 'Κατοικία-διαμέρισμα', 'Επαγγελματική στέγη', 'Γεωργικά κτίρια', 'Γεωργικό κτίριο', 'Γεωργικά κτήρια',
+						 'Γεωργικό κτήρια', 'κτηνοτροφικά κτίρια', 'κτηνοτροφικά κτήρια', 'κτηνοτροφικό κτίριο',
+						 'κτηνοτροφικό κτήριο', 'Θέσης στάθμευσης', 'Θέσεις στάθμευσης', 'Θέση στάθμευσης',
+						 'Θέσεων στάθμευσης', 'Βιομηχανικά κτήρια', 'Βιομηχανικά κτίρια', 'Βιομηχανικοί χώροι',
+						 'Βιομηχανικών Χώρων', 'τουριστικές εγκαταστάσεις', 'τουριστικών εγκαταστάσεων', 'Εκπαιδευτήρια',
+						 'Εκπαιδευτήριο', 'Αθλητικές εγκαταστάσεις', 'Αθλητικών εγκαταστάσεων']
+kw_size_of_land_list = ['οικοπέδου', 'τμήμα οικοπέδου εμβαδού', 'τμήμα οικοπέδου επιφάνειας',
+						'αποτελεί τμήμα μεγαλύτερου οικοπέδου ολικού εμβαδού', 'αποτελεί τμήμα μεγαλύτερου οικοπέδου',
+						'επί οικοπέδου εκτάσεως', 'κείται επί οικοπέδου εκτάσεως', 'μετά του οικοπέδου εκτάσεως',
+						'επί οικοπέδου εκτάσεως', 'επί του οικοπέδου εκτάσεως', 'το κτίσμα είναι σε οικόπεδο',
+						'ανήκει σε οικόπεδο', 'κυριότητας ενός οικοπέδου εκτάσεως', 'συνιδιοκτησίας επί του οικοπέδου',
+						'ιδιοκτησίας επί του οικοπέδου', 'κτισμένη πάνω σε οικόπεδο μ.τ.', 'κτισμένη πάνω σε οικόπεδο']
+kw_percentage_of_ownership_list = ['δικαιώματος της πλήρους κυριότητας σε ποσοστό', 'πλήρης κυριότητας σε ποσοστό',
+								   'πλήρη κυριότητα σε ποσοστό', 'ποσοστό συνιδιοκτησίας εξ αδιαιρέτου',
+								   'συμμετέχει στο κοινό οικόπεδο με ποσοστό συνιδιοκτησίας']
+kw_notary_public_list = ['Η Συμβολαιογράφος', 'O Συμβολαιογράφος']
+
+# Train each text seperately
+for i in txt_filepaths[:30]:
+	with open(i, 'r',
+			  encoding="utf8") as f:
+		data = f.read().replace('\n', ' ')
+	structure_training_data(data, kw_auction_date_list, kw_tax_id_of_debtor_list, kw_auction_id_list, kw_eauction_website_list,
+							kw_foreclosure_id_list, kw_accelerator_list, kw_tax_id_accelerator_list, kw_property_type_list,
+							kw_size_of_land_list, kw_percentage_of_ownership_list, kw_notary_public_list)
+
+collective_dict
+
+# define our training data to TRAIN_DATA
+TRAIN_DATA = collective_dict['TRAINING_DATA']
+
+# create a blank model
+nlp = spacy.blank('en')
+
+def create_training(TRAIN_DATA):
+    db = DocBin()
+    for text, annot in tqdm(TRAIN_DATA):
+        doc = nlp.make_doc(text)
+        ents = []
+
+        # create span objects
+        for start, end, label in annot["entities"]:
+            span = doc.char_span(start, end, label=label, alignment_mode="contract")
+
+            # skip if the character indices do not map to a valid span
+            if span is None:
+                print("Skipping entity.")
+            else:
+                ents.append(span)
+                # handle erroneous entity annotations by removing them
+                try:
+                    doc.ents = ents
+                except:
+                    # print("BAD SPAN:", span, "\n")
+                    ents.pop()
+        doc.ents = ents
+
+        # pack Doc objects into DocBin
+        db.add(doc)
+    return db
+
+TRAIN_DATA_DOC = create_training(TRAIN_DATA)
+
+# Export results (here I add it to a TRAIN_DATA folder within the directory)
+TRAIN_DATA_DOC.to_disk("C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/train_data/train_data.spacy")
+
+"""
+1. Follow the link: https://spacy.io/usage/training#config and selecte ‘ner’ in the Components section of the widget. 
+Copy/paste the full contents of the widget into a file named base_config.cfg within your folder directory.
+
+2. Open the base_config.cfg file. All you need to change is the train and dev variables at the top.
+Dev is reserved for the validation dataset. However, because I just want to train the model for demonstration purposes, 
+I added the same path to both train and dev. (Note: this will result in 100% training accuracy)
+
+3. Open your CLI and cd over into the directory of base_config.cfg and simply copy/paste this command:
+python -m spacy init fill-config base_config.cfg config.cfg
+
+4. Run the following to begin training: 
+python -m spacy train config.cfg --output C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/results
+"""
+
+# Test out the model we trained on a brand new piece of text.
+with open(txt_filepaths[67], 'r', encoding="utf8") as f:
+	test_1 = f.read().replace('\n', ' ')
+
+# load the trained model
+nlp_output = spacy.load("C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/results/model-best")
+
+# pass our test instance into the trained pipeline
+doc = nlp_output(test_1)
+
+'''# customize the label colors
+colors = {"auction_date": "linear-gradient(90deg, #E1D436, #F59710)", "tax_id_of_debtor": "linear-gradient(90deg, #E1D436, #F59710)"}
+options = {"ents": ["auction_date", "tax_id_of_debtor"], "colors": colors}
+
+# visualize the identified entities
+html = displacy.render(doc, style="ent", options=options)'''
+
+# print out the identified entities
+for ent in doc.ents:
+    if ent.label_ == "auction_date":
+        print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "tax_id_of_debtor":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "auction_id":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "eauction_website":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "foreclosure_id":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "accelerator":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "tax_id_accelerator":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "property_type":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "size_of_land":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "percentage_of_ownership":
+		print(ent.text, ent.label_)
+for ent in doc.ents:
+	if ent.label_ == "notary_public":
+		print(ent.text, ent.label_)
--- object_detect.py
+++ object_detect.py
@@ -1,4 +1,3 @@
-from pdf2image import convert_from_path
 import os
 import pytesseract
 import cv2
@@ -14,7 +13,7 @@ e.x. C:\Program Files\Tesseract-OCR
 test data and language data folders must have the names "langdata" and "tessdata".
 Finally, configure tessdata path as shown below and use it as a parameter when tesseract is called in the code."""
 
-
+start = time.time()
 # Set the tesseract test data path in the code
 tessdata_dir_config = '--tessdata-dir "C:/Program Files/Tesseract-OCR/tessdata"'
 # Set the tesseract path in the code
@@ -22,83 +21,19 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 # Poppler dependecy is needed for pdf2image to work
 poppler = r'C:\Program Files (x86)\poppler-0.68.0\bin'
 # Extract images from pdf and save to a directory
-pdf_path = r'C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/pdfs/Κυλινδρόμυλοι Α.Ε. κλπ. απόφαση 954 ΚΠολΔ.pdf'
-directory = 'C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/pages/'
+directory = 'C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/pages/'
 
 
-def pdf_to_txt_pdf2image(pdf_path):
-    start = time.time()
-    pages = convert_from_path(pdf_path, 500, poppler_path=poppler)
-    count = 0
-    for page in pages:
-        page.save('C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/pages/' + 'page-%d.png' % count, 'PNG')
-        count += 1
-
-    # Save full paths of pages in a list
-    file_paths = []  # List which will store all of the full filepaths.
-    # Walk the tree.
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            # Join the two strings in order to form the full filepath.
-            filepath = os.path.join(root, filename)
-            file_paths.append(filepath)  # Add it to the list.
-
-    text_name_from_pdf = 'C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/texts/' + \
-                         os.path.splitext(os.path.basename(pdf_path))[0] + '.txt'
-    # A text file is created and flushed
-    txt_file = open(text_name_from_pdf, "w", encoding="utf-8")
-    # OpenCV will handle image processing
-    for path in file_paths:
-        img = cv2.imread(path)
-        # Rescaling the image (it's recommended if you’re working with images that have a DPI of less than 300 dpi)
-        img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
-        # Convert the image to gray scale
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        # Applying dilation and erosion to remove the noise (you may play with the kernel size depending on your data set)
-        #kernel = np.ones((1, 1), np.uint8)
-        #img = cv2.dilate(img, kernel, iterations=1)
-        #img = cv2.erode(img, kernel, iterations=1)
-        # Applying blur, which can be done by using one of the following lines
-        # (each of which has its pros and cons, however, median blur and bilateral filter usually perform better than gaussian blur.)
-        #img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-        #img = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-        #img = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-        #img = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-
-        #img = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-
-        #img = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-        if img is None:
-            txt_file = open(text_name_from_pdf, "a", encoding="utf-8")
-            text = "---Could not read the image.---"
-            txt_file.write(text)
-            txt_file.close()
-        else:
-            txt_file = open(text_name_from_pdf, "a", encoding="utf-8")
-            text = pytesseract.image_to_string(img, lang="ell", config=tessdata_dir_config)
-            txt_file.write(text)
-            txt_file.close()
-    end = time.time()
-    print('Pdf was converted to txt successfully.')
-    print("Pdf to text conversion took {:.1f} seconds".format(end - start))
-    print(text_name_from_pdf)
-    return text_name_from_pdf
-
-
-def pdf_to_txt_pymupdf(pdf_path):
+def pdf_to_txt_pymupdf(pdf_filepath):
     start = time.time()
 
     # open your file
-    doc = fitz.open(pdf_path)
+    doc = fitz.open(pdf_filepath)
     # iterate through the pages of the document and create a RGB image of the page
     for page in doc:
         pix = page.get_pixmap()
-        pix.save("C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/pages/page-%i.png" % page.number)
-
+        pix.save(
+            "C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/pages/page-%i.png" % page.number)
 
     # Save full paths of pages in a list
     file_paths = []  # List which will store all of the full filepaths.
@@ -109,8 +44,8 @@ def pdf_to_txt_pymupdf(pdf_path):
             filepath = os.path.join(root, filename)
             file_paths.append(filepath)  # Add it to the list.
 
-    text_name_from_pdf = 'C:/Users/kostas.skepetaris/PycharmProjects/object_detection_nlp/app/static/texts/' + \
-                         os.path.splitext(os.path.basename(pdf_path))[0] + '.txt'
+    text_name_from_pdf = 'C:/Users/kostas.skepetaris/PycharmProjects/web_retrieve_data/static/texts/' + \
+                         os.path.splitext(os.path.basename(pdf_filepath))[0] + '.txt'
     # A text file is created and flushed
     txt_file = open(text_name_from_pdf, "w", encoding="utf-8")
     # OpenCV will handle image processing
@@ -120,24 +55,24 @@ def pdf_to_txt_pymupdf(pdf_path):
         img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
         # Convert the image to gray scale
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         # Applying dilation and erosion to remove the noise (you may play with the kernel size depending on your data set)
-        #kernel = np.ones((1, 1), np.uint8)
-        #img = cv2.dilate(img, kernel, iterations=1)
-        #img = cv2.erode(img, kernel, iterations=1)
+        # kernel = np.ones((1, 1), np.uint8)
+        # img = cv2.dilate(img, kernel, iterations=1)
+        # img = cv2.erode(img, kernel, iterations=1)
         # Applying blur, which can be done by using one of the following lines
         # (each of which has its pros and cons, however, median blur and bilateral filter usually perform better than gaussian blur.)
-        #img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        # img = cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-        #img = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        # img = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-        #img = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        # img = cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-        #img = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+        # img = cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
-        #img = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+        # img = cv2.adaptiveThreshold(cv2.bilateralFilter(img, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
-        #img = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+        # img = cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
         if img is None:
             txt_file = open(text_name_from_pdf, "a", encoding="utf-8")
             text = "---Could not read the image.---"
@@ -145,7 +80,7 @@ def pdf_to_txt_pymupdf(pdf_path):
             txt_file.close()
         else:
             txt_file = open(text_name_from_pdf, "a", encoding="utf-8")
-            text = pytesseract.image_to_string(img, lang="ell", config=tessdata_dir_config)
+            text = pytesseract.image_to_string(img, lang="eng+ell", config=tessdata_dir_config)
             txt_file.write(text)
             txt_file.close()
     end = time.time()
@@ -155,4 +90,5 @@ def pdf_to_txt_pymupdf(pdf_path):
     return text_name_from_pdf
 
 
-pdf_to_txt_pymupdf(pdf_path)
+pdf_filepath = 'C:\\Users\\kostas.skepetaris\\PycharmProjects\\web_retrieve_data\\static\\pdfs\\deltio_nomikon\\report171056.pdf'
+pdf_to_txt_pymupdf(pdf_filepath)
